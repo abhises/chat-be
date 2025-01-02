8f46be43bf716149dc4c7ec591cee9b62dc30fac
abhises <abhisespoudyal@gmail.com>
2025-01-02 17:17:36 +0545

    register added

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { registerUser } = require("../Controllers/userController");
 
-router.get("/register", (req, res) => {
-  res.send("register");
-});
+router.post("/register", registerUser);
 
 module.exports = router;
