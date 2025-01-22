79a7479f3b9662520d6e352dc389e1a749305054
abhises <abhisespoudyal@gmail.com>
2025-01-22 17:55:21 +0545

    combining express and socket

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const userRoute = require("./Routes/userRoute");
 const chatRoute = require("./Routes/chatRoute");
 const messageRoute = require("./Routes/messageRoute");
+const { Server } = require("socket.io");
 
 require("dotenv").config();
 
@@ -17,9 +18,12 @@ app.use("/api/chat", chatRoute);
 app.use("/api/message", messageRoute);
 
 const port = process.env.PORT || 5000;
-app.listen(port, (req, res) => {
+
+const expressServer = app.listen(port, (req, res) => {
   console.log(`server running at port:${port}`);
 });
+
+const io = new Server(expressServer, { cors: process.env.CLIENT_URL });
 
 // MongoDB Connection
 const mongoURI = process.env.DB_URL; // Replace with your MongoDB URI
@@ -27,3 +31,38 @@ mongoose
   .connect(mongoURI)
   .then(() => console.log("MongoDB database connected..."))
   .catch((err) => console.error("MongoDB connection error:", err));
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+  console.log("new Connection...", socket.id);
+
+  socket.on("addNewUser", (userId) => {
+    !onlineUsers.some((user) => user.userId === userId) &&
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      });
+
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+  // console.log("onlineusers", onlineUsers);
+
+  socket.on("sendMessage", (message) => {
+    const user = onlineUsers.find(
+      (user) => user.userId === message.recipientId
+    );
+    if (user) {
+      io.to(user.socketId).emit("getMessage", message);
+      io.to(user.socketId).emit("getNotification", {
+        senderId: message.senderId,
+        isRead: false,
+        date: new Date(),
+      });
+    }
+  });
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
+});
